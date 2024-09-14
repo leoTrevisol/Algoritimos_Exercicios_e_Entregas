254fdd07469614942d20ee9519a23aa751eefdf3
leoTrevisol <leonardotrevisol430@gmail.com>
2024-09-14 13:19:32 -0300

    att

---
+++ Lista3/ex1.c
@@ -0,0 +1,48 @@
+/*
+ * Programa: ex1.c
+ * Descrição: Escreva um programa que leia dois números e efetue uma divisão, mas somente se o divisor for
+diferente de zero; quando isto ocorrer, é mostrada uma mensagem de erro apropriada.
+ * Autor: Leonardo Trevisol
+ * Data de criação: 11.09.2024
+ * Última modificação: 11.09.2024
+ *
+ * Observações:
+ * - Por que n eh possivel colocar a comando break em um laço if/else?(n consegui far isso).
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+float divisao(float dividendo, float divisor);
+int main()
+{
+
+    float n1, n2, valorFinal;
+
+    printf("Digite um dividendo: ");
+    scanf("%f", &n1);
+
+    printf("Digite um divisor: ");
+    scanf("%f", &n2);
+
+    valorFinal = divisao(n1, n2);
+    if (n2 == 0)
+    {
+        printf("Erro, essa divisao eh impossivel!");
+    }
+    else
+    {
+        printf("A difisao de %.2f por %.2f eh: %.2f", n1, n2, valorFinal);
+    }
+
+    return 0;
+}
+
+float divisao(float dividendo, float divisor)
+{
+
+    float resultado;
+    resultado = dividendo / divisor;
+    return resultado;
+}
+//ola
---
+++ Lista3/ex2.c
@@ -0,0 +1,47 @@
+/*
+ * Programa: ex1.c
+ * Descrição: Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que A
++ C.
+ * Autor: Leonardo Trevisol
+ * Data de criação: 11.09.2024
+ * Última modificação: 11.09.2024
+ *
+ * Observações:
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int somatorio(int a, int b, int c);
+int main()
+{
+
+    int valorA, valorB, valorC;
+
+    printf("Digite o valor A: ");
+    scanf("%d", &valorA);
+
+    printf("Digite o valor B: ");
+    scanf("%d", &valorB);
+
+    printf("Digite o valor C: ");
+    scanf("%d", &valorC);
+
+    somatorio(valorA, valorB, valorC);
+
+    return 0;
+}
+
+int somatorio(int a, int b, int c)
+{
+
+    if (a + b < a + c)
+    {
+        printf("%d + %d eh menor que %d + %d.", a, b, a, c);
+    }
+    else
+    {
+        printf("%d + %d eh maior ou igual a %d + %d.", a, b, a, c);
+    }
+}
