935d6e91a1a430ac262f81938d0bc02f527d9e74
Leonardo Trevisol <leonardotrevisol430@gmail.com>
2024-09-17 20:46:20 -0300

    Update ex2.c

--- Lista3/ex2.c
+++ Lista3/ex2.c
@@ -1,5 +1,5 @@
 /*
- * Programa: ex1.c
+ * Programa: ex2.c
  * Descrição: Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que A
 + C.
  * Autor: Leonardo Trevisol
