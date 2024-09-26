fcd13df031737fb41b289bcd5ac92cef561edc90
Leonardo Trevisol <leonardotrevisol430@gmail.com>
2024-09-25 21:27:11 -0300

    att lista3

---
+++ Lista3/ex12.c
@@ -0,0 +1,65 @@
+/*
+ * Programa: ex6.c
+ * Descrição:A confederação brasileira de natação irá promover eliminatórias para o próximo mundial. Fazer um
+algoritmo que receba a idade de um nadador e imprima a sua categoria segundo a tabela a seguir:
+Categoria Idade
+Infantil A 5-7 anos
+Infantil B 8-10 anos
+Juvenil A 11-13 anos
+Juvenil B 14-17 anos
+Sênior Maiores de 18 anos
+ * Autor: Leonardo Trevisol
+ * Data de criação: 25.09.2024
+ * Última modificação: 25.09.2024
+ *
+ * Observações:
+ *
+ */
+
+#include <stdio.h>
+
+void verificadorIdade(int ageAth);
+int main()
+{
+    int count = 1, idadeAtleta;
+    while(count){
+    printf("CADASTRO PARA ELIMINATORIAS DO MUNDIAL DE NATACAO.\n\n");
+    printf("Digite a idade do nadador: ");
+    scanf("%d",&idadeAtleta);
+    if (idadeAtleta <= 4)
+    {
+        printf("Idade indisponivel.\n");
+    }else{ 
+    verificadorIdade(idadeAtleta);
+    printf("\nPara sair digite 0.\nPara realizar outro cadastro digite 1.\n");
+    scanf("%d",&count);
+    }
+    }
+
+    return 0;
+}
+
+void verificadorIdade(int ageAth){
+
+    if (ageAth <= 7)
+    {
+        printf("A categoria do atleta eh Infantil A.");
+    }
+    else if (ageAth <= 10)
+    {
+        printf("A categoria do atleta eh Infantil B.");
+    }
+    else if (ageAth <= 13)
+    {
+        printf("A categoria do atleta eh Juvenil A.");
+    }
+    else if (ageAth <= 17)
+    {
+        printf("A categoria do atleta eh Juvenil B.");
+    }
+    else
+    {
+        printf("A categoria do atleta eh Sênior.");
+    }
+    
+}
---
+++ Lista3/ex13.c
@@ -0,0 +1,24 @@
+/*
+ * Programa: ex6.c
+ * Descrição:Faça um programa que leia a nota do Grau A e do Grau B do aluno e calcule a média final conforme
+as regras da Unisinos. Imprima a média final na tela e diga se o aluno passou por média ou se ficou
+em recuperação (grau C). Se o aluno ficou em recuperação, pergunte se ele quer substituir o Grau
+A ou o Grau B (ele deve digitar ‘a’ ou ‘b’). Leia a nota do Grau C, recalcule a média de acordo com o
+grau substituído e imprima na tela o resultado, informando se ele foi aprovado ou reprovado.
+ * Autor: Leonardo Trevisol
+ * Data de criação: 25.09.2024
+ * Última modificação: 25.09.2024
+ *
+ * Observações:
+ *
+ */
+
+#include <stdio.h>
+
+
+int main()
+{
+    float notaProva;
+
+    return 0;
+}
